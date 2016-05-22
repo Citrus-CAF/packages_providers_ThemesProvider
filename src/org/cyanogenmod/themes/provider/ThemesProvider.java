LineageOS/android_packages_providers_ThemesProvider

themesprovider: Fix priority of verifier thread
 * Thread.MIN_PRIORITY is not the same as an Android
   background process. The former has a nice of 1, and
   the latter as a nice of 10. In this case, we pick
   THREAD_PRIORITY_LOWEST, which is actually 19.

Change-Id: Ic4d77f40915490a95cee01f57a9cf7f61c715d55

src/org/cyanogenmod/themes/provider/ThemesProvider.java
@@ -31,6 +31,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.Process;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
@@ -493,7 +494,7 @@ public class ThemesProvider extends ContentProvider {
         }
 
         public void run() {
-            android.os.Process.setThreadPriority(Thread.MIN_PRIORITY);
+            Process.setThreadPriority(Process.THREAD_PRIORITY_LOWEST);
 
             long start;
 

Citrus-CAF/packages_providers_ThemesProvider

   the latter as a nice of 10.
Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

src/org/cyanogenmod/themes/provider/ThemesProvider.java
@@ -35,6 +35,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.Process;
 import android.preference.PreferenceManager;
 import android.provider.ThemesContract;
 import android.provider.ThemesContract.MixnMatchColumns;
@@ -485,7 +486,7 @@ public class ThemesProvider extends ContentProvider {
         }
 
         public void run() {
-            android.os.Process.setThreadPriority(Thread.MIN_PRIORITY);
+            Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
 
             long start;
 